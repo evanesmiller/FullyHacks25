f7dc014e513f2fa0bcd0387d99c49095db00dff2
Evan Miller <149550738+evanesmiller@users.noreply.github.com>
2025-04-12 22:50:25 -0700

    Add files via upload

---
+++ pages/About.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+function About(){
+    return (
+        <div style={{display: "block", width: "100%"}}>
+            <Navbar />
+        </div>
+    );
+}
+
+export default About;
---
+++ pages/Chatbot.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+function Chatbot(){
+    return (
+        <div style={{display: "block", width: "100%"}}>
+           <Navbar /> 
+        </div>
+    );
+}
+
+export default Chatbot;
